a03c94ba5baadf40a80ba9d5ea5a21a4ae3dc3ea
Vidmantas <vidmantas.luneckas@gmail.com>
2025-12-02 12:33:26 +0200

    .

--- examples/file-upload.js
+++ examples/file-upload.js
@@ -24,7 +24,7 @@ function getBase64String(data) {
  * @returns {UploadLocation}
  */
 async function getUploadLocation(apikey, filename) {
-    const sourcedata = getBase64String(JSON.stringify({ id: 'unique-document-id' }));
+    const sourcedata = getBase64String(JSON.stringify({ id: 'unique-document-id', customdata: 'custom-data' }));
 
     const url = new URL(`https://app.ivesk.lt/api/pub/uploadlocation/${encodeURIComponent(filename)}?${encodeURIComponent(sourcedata)}&processlines=false&splitdocuments=true&checkduplicates=true&rejectnoninvoices=true&warehouse=WHS&allocation=ALLOC&tags=tag1,tag2,tag3`);
     const response = await fetch(url, {
